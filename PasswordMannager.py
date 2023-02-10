611f1ac418441af5aba0b1de8ffe2bb5b88b0f26
AtesiT <atesit@mail.ru>
2023-02-10 12:03:14 +0300

    almost did it

--- PasswordMannager.py
+++ PasswordMannager.py
@@ -1,11 +1,33 @@
-# master_pwd = input('Что такое "мастер" паролей? ')
+from cryptography.fernet import Fernet
+
+'''
+def write_key():
+    key = Fernet.generate_key()
+    with open('key.key', 'wb') as key_file:
+        key_file.write(key) 
+'''
+
+
+def load_key():
+    file = open('key.key', 'rb')
+    key = file.read()
+    file.close()
+    return key
+
+
+master_pwd = input('Какой "мастер" паролей? ')
+key = load_key() + master_pwd.encode()
+fer = Fernet(key)
+
+
 
 def view():
     with open('passwords.txt', 'r') as f:
         for line in f.readlines():
             data = line.rstrip()
             user, passw = data.split(' | ')
-            print('Логин: ', user, '| Пароль: ', passw)
+            print('Логин: ', user, '| Пароль: ', fer.decrypt(pwd.encode()).decode())
+
 
 
 def add():
@@ -13,7 +35,7 @@ def add():
     pwd = input('Пароль: ')
     
     with open('passwords.txt', 'a') as f:
-        f.write(name + ' | ' + pwd + '\n')
+        f.write(name + ' | ' + fer.encrypt(pwd.encode()).decode() + '\n')
 
 
 while True:
